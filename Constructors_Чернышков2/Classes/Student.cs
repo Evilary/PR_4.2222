57f49a25c8f421568f6928529865ae432ee52cf7
student-a502 <student-a502@STUD-RM14-A502>
2025-10-24 12:49:38 +0500

    Разработанный класс Student. Чернышков

---
+++ Constructors_Чернышков2/Classes/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_Чернышков2.Classes
+{
+    public class Student
+    {
+        public string Firstname = "";
+        public string Lastname = "";
+        public string Surname = "";
+        public bool Schoolarship = false;
+        public int Course = 4;
+
+        public Student(string Firstname, string Lastname, string Surname)
+        {
+            this.Firstname = Firstname;
+            this.Lastname = Lastname;
+            this.Surname = Surname;
+        }
+
+        public Student(string Firstname, string Lastname, string Surname, bool Schoolarship) 
+            : this(Firstname,Lastname,Surname) {
+            this.Schoolarship = Schoolarship;
+        }
+
+        public Student(string Firstname, string Lastname, string Surname, bool Schoolarship, int Course)
+            : this(Firstname, Lastname, Surname, Schoolarship)
+        {
+            this.Course = Course;
+        }
+
+        public string GetFIO()
+        {
+            return $"{Lastname}{Firstname}{Surname}";
+        }
+
+    }
+}
